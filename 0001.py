ec3932ef6f7c6f6b1b50552ffb01bf6c9bc958fc
Mani <mani.ahmadi@ssense.com>
2019-03-08 15:05:44 -0500

    add comment

--- 0001.py
+++ 0001.py
@@ -1,5 +1,6 @@
 # complement of x
 # ~x  This is the same as -x - 1. 
+# this is useful for looping through a list from 2 sides at the same time with 0 and -1, 1 and -2, with i and ~i
 print('~0 produces:', ~0) # ~0 produces: -1
 print('~1 produces:', ~1) # ~1 produces: -2
 print('~(-1) produces:', ~(-1)) # ~(-1) produces: 0
