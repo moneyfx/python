75c0ae33d243e7b117b1ee6f103011467883112b
Mani <mani.ahmadi@ssense.com>
2019-03-08 15:58:51 -0500

    print examples

--- 0002_print.py
+++ 0002_print.py
@@ -2,9 +2,10 @@ a = 'string1'
 b = 'string2'
 c = 'string3'
 
-# these two are the same
+# these three are the same
 print(a, b, c) # string1 string2 string3
 print(a, b, c, sep=' ', end='\n') # string1 string2 string3
+print(a, b, c, end='\n', sep=' ') # string1 string2 string3
 
 print(a, b, c, sep='##') # string1##string2##string3
 
