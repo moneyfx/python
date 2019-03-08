3f541bf57bdab5fb7465acb54cae211e388b3b12
Mani <mani.ahmadi@ssense.com>
2019-03-08 15:03:28 -0500

    bitwise operator

---
+++ 0001.py
@@ -0,0 +1,10 @@
+# complement of x
+# ~x  This is the same as -x - 1. 
+print('~0 produces:', ~0) # ~0 produces: -1
+print('~1 produces:', ~1) # ~1 produces: -2
+print('~(-1) produces:', ~(-1)) # ~(-1) produces: 0
+
+# Left shift
+print('100 << 1 produces', (100 << 1)) # 100 << 1 produces 200
+# right shift
+print('100 >> 1 produces', (100 >> 1)) # 100 >> 1 produces 50
