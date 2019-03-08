c67e3b5a175b9a9cad1295711a7cd2cb0620a404
Mani <mani.ahmadi@ssense.com>
2019-03-08 16:59:01 -0500

    new list

--- 0003_list.py
+++ 0003_list.py
@@ -1,3 +1,8 @@
+my_list = []
+my_list_2 = list()
+print(my_list) # []
+print(my_list_2) # []
+
 # convert string to list(char array)
 chars_list = list('hello')
 print(chars_list) # ['h', 'e', 'l', 'l', 'o']
