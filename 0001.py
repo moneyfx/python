53557943486ea1023aa6715f3606b8a02f4f7490
Mani <mani.ahmadi@ssense.com>
2019-03-08 15:12:56 -0500

    add comment

--- 0001.py
+++ 0001.py
@@ -5,6 +5,8 @@ print('~0 produces:', ~0) # ~0 produces: -1
 print('~1 produces:', ~1) # ~1 produces: -2
 print('~(-1) produces:', ~(-1)) # ~(-1) produces: 0
 
+# left shifting 'n' times means multiplying by 2^n
+
 # Left shift once
 print('100 << 1 produces', (100 << 1)) # 100 << 1 produces 200
 # right shift once
