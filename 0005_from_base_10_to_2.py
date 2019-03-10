ad1e9374c01c5a80882c592337667f9f54212a3c
Mani <mani.ahmadi@ssense.com>
2019-03-09 19:26:25 -0500

    from decimal to binary

---
+++ 0005_from_base_10_to_2.py
@@ -0,0 +1,22 @@
+a = -34
+sign = '0b' if a >= 0 else '-0b'
+
+print(bin(a))
+
+l = []
+while a != 0:
+    r = a % 2
+    l.append(r)
+    a = int(a / 2)
+
+#hanle zero
+if len(l) == 0:
+    l.append(0)
+
+#print(l[::-1])
+#print(list(reversed(l)))
+
+# reverse in place
+l.reverse()
+
+print(sign + ''.join(str(x) for x in l))
