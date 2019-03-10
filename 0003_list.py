1a95a1bb660feb9d0ee99b6a167ba05790ac8926
Mani <mani.ahmadi@ssense.com>
2019-03-09 19:45:36 -0500

    reverse

--- 0003_list.py
+++ 0003_list.py
@@ -13,3 +13,12 @@ print(length_of_list) # 5
 # convert list of chars(not int) to string
 str_from_list = ''.join(chars_list)
 print(str_from_list) # hello
+
+list_of_ints = [1, 2, 3]
+
+print(list_of_ints[::-1]) # [3, 2, 1]
+print(list(reversed(list_of_ints))) # [3, 2, 1]
+
+#reverse the list in place:
+list_of_ints.reverse()
+print(list_of_ints) # [3, 2, 1]
